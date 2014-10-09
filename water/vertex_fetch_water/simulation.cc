6e4d7224e4ca9e52dd0fdc5b30cea4b5fb8f3e95
lenxyang <lenxyang@gmail.com>
2014-10-09 15:06:44 +0800

    .

--- water/vertex_fetch_water/simulation.cc
+++ water/vertex_fetch_water/simulation.cc
@@ -5,13 +5,30 @@
 #include "azer/render/render.h"
 
 #include "water_simulation.afx.h"
+#include "quad.afx.h"
 #define EFFECT_GEN_DIR "out/dbg/gen/sandbox/water/vertex_fetch_water/"
 #define SHADER_NAME "water_simulation.afx"
+#define SHADER2_NAME "quad.afx"
 
 bool WaterSimulation::Init(azer::RenderSystem* rs) {
   if (!InitVertex(rs)) {
     return false;
   }
+
+  azer::ShaderArray shaders;
+  CHECK(azer::LoadVertexShader(EFFECT_GEN_DIR SHADER2_NAME ".vs", &shaders));
+  CHECK(azer::LoadPixelShader(EFFECT_GEN_DIR SHADER2_NAME ".ps", &shaders));
+  QuadEffect* effect = new QuadEffect(shaders.GetShaderVec(), rs);
+  quad_effect_.reset(effect);
+
+  for (uint32 i = 0; i < arraysize(target_); ++i) {
+    target_[i].reset(new azer::TexRenderTarget(800, 600));
+    target_[i]->Init(rs);
+    target_[i]->Begin(azer::Vector4(0.0f, 0.0f, 0.0f, 0.0f));
+  }
+
+  azer::Renderer* renderer = rs->GetDefaultRenderer();
+  renderer->Use();
   return true;
 }
 
@@ -46,7 +63,17 @@ bool WaterSimulation::InitVertex(azer::RenderSystem* rs) {
 }
 
 void WaterSimulation::Render(azer::Renderer* renderer) {
+  renderer->Use();
+  quad_effect_->Use(renderer);
+  renderer->DrawIndex(vb_.get(), ib_.get(), azer::kTriangleList);
+}
+
+void WaterSimulation::CalcSimulation() {
+  azer::Vector4 color(0.0f, 0.0f, 0.0f, 0.0f);
+  azer::TexRenderTargetPtr prev = target_[index];
+  azer::Renderer* renderer = target_[++index_]->Begin(color);
   WaterSimulationEffect* effect = (WaterSimulationEffect*)effect_.get();
+  effect->SetPrev(prev->GetRTTex());
   effect->Use(renderer);
-renderer->DrawIndex(vb_.get(), ib_.get(), azer::kTriangleList);
+  renderer->DrawIndex(vb_.get(), ib_.get(), azer::kTriangleList);
 }
--- water/vertex_fetch_water/simulation.h
+++ water/vertex_fetch_water/simulation.h
@@ -2,18 +2,21 @@
 
 #include "base/basictypes.h"
 #include "azer/render/render.h"
+#include "azer/render/util/tex_render_target.h"
 
 class WaterSimulation {
  public:
-  WaterSimulation() {}
+  WaterSimulation() : index_(0) {}
   bool Init(azer::RenderSystem* rs);
   void Render(azer::Renderer* renderer);
  private:
+  void CalcSimulation();
   bool InitVertex(azer::RenderSystem* rs);
-  azer::TexturePtr prev_;
-  azer::TexturePtr curr_;
+  int index_;
+  azer::TexRenderTargetPtr target_[2];
   azer::VertexBufferPtr vb_;
   azer::IndicesBufferPtr ib_;
   azer::EffectPtr effect_;
+  azer::EffectPtr quad_effect_;
   DISALLOW_COPY_AND_ASSIGN(WaterSimulation);
 };
